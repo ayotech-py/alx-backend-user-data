95be037464883b00dae4bcb5c64350331d11aaa2
ayotech-py <ajaomalik2018@gmail.com>
2023-05-08 23:46:08 +0000

    user authentication

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+This file creates a user model
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(120), nullable=False, unique=True)
+    hashed_password = Column(String(128), nullable=False)
+    session_id = Column(String(128), nullable=True)
+    reset_token = Column(String(128), nullable=True)
+
+    def __repr__(self):
+        return f"<User(email='{self.email}')>"
